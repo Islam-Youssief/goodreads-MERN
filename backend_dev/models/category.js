f8311d1ccc1b07a325f32d44d62ecec5b6d554c8
Islam Youssief <islam.youssief@gmail.com>
2019-03-07 01:32:28 +0200

    Removed unused duplicate files

--- backend_dev/models/category.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const categorySchema = new mongoose.Schema({
-    name: String
-});
-
-const Category = mongoose.model('catogries', categorySchema);
-module.exports = Category;
-
--- models/category.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const categorySchema = new mongoose.Schema({
-    name: String
-});
-
-const Catogries = mongoose.model('catogries', categorySchema);
-module.exports = Catogries;
-
